cb7cf5d5a7792211ca654724df9a2826231b0b1d
Julie Reynal <rjulie.pro@gmail.com>
2024-02-29 17:13:23 +0100

    Homepage with cards items done

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -5,7 +5,6 @@ function Banner({ title }) {
       style={{
         backgroundImage:
           'linear-gradient( rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.6)), url(/assets/landscape-homepage.jpg)',
-        background: 'linear-gradient(#e66465, #000);',
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
         width: '1240px',
--- src/components/CardItem/index.jsx
+++ src/components/CardItem/index.jsx
@@ -1,11 +1,17 @@
 function CardItem({ title, cover }) {
-	return (
-		<li className=''>
-			<span className=''>{title}</span>
-			<img className='' src={cover} alt={`${title} cover`} />
-			{title}
-		</li>
-	)
+  return (
+    <div
+      className="card-item"
+      style={{
+        backgroundImage: `linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, rgba(10, 10, 10, 0.00) 42%, rgba(4, 4, 4, 0.20) 99.99%, rgba(0, 0, 0, 0.50) 100%), url('${cover}')`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        width: '340px',
+      }}
+    >
+      <h2>{title}</h2>
+    </div>
+  )
 }
 
 export default CardItem
