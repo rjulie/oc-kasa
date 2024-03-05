cf38dbbb2cc28f4145651b7b773b9f4656cee189
Julie Reynal <rjulie.pro@gmail.com>
2024-03-05 23:28:03 +0100

    About: dropdown update with bolean

--- src/components/OpenDropdown/index.jsx
+++ src/components/OpenDropdown/index.jsx
@@ -2,34 +2,40 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react'
+import PropTypes from 'prop-types'
 
-function OpenDropdown({ title, description, index }) {
+function OpenDropdown({ title, description = null }) {
   const [open, setOpen] = useState(null)
 
-  const toggle = (index) => {
-    if (open === index) {
+  const toggle = () => {
+    if (open === true) {
       return setOpen(null)
     }
-    setOpen(index)
+    setOpen(true)
   }
 
   return (
     <div className="dropdown">
-      <div className="dropdown-item" onClick={() => toggle(index)}>
+      <div className="dropdown-item" onClick={() => toggle()}>
         <h2>{title}</h2>
         <span>
-          {open === index ? (
+          {open === true ? (
             <FontAwesomeIcon icon={faChevronDown} />
           ) : (
             <FontAwesomeIcon icon={faChevronUp} />
           )}
         </span>
       </div>
-      <div className={open === index ? 'dropdown-text show' : 'dropdown-text'}>
+      <div className={open === true ? 'dropdown-text show' : 'dropdown-text'}>
         <p>{description}</p>
       </div>
     </div>
   )
+}
+
+OpenDropdown.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
 }
 
 export default OpenDropdown
